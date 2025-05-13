b339830f65e645a479b2b585fcb1a78cb9f55282
Luiz Felipe <108752298+4artxd@users.noreply.github.com>
2025-05-13 12:23:43 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,51 @@
+<<<<<<< HEAD
+const CACHE_NAME = 'calculadora-cache-v1';
+const URLS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+=======
+const CACHE_NAME = 'calculadora-cache-v1';
+const URLS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+>>>>>>> 59de75cdb46af89e3899c7a0b08c3c9a8ac1230f
